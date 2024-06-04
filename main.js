9b896ef66666e536e9881569f317272f47a00110
Peter Farrow <pnfarrow@gmail.com>
2024-06-04 10:07:36 -0600

    about halfway done

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-import './style.css';
+import "./style.css";
+import getRandomWord from "./src/randomWord.js";
+import setSharkImage from "./src/sharkImage.js";
+import { setupWord, isLetterInWord, revealLetterInWord } from "./src/word.js";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <section id="shark-img"></section>
 
   <section id="game-status"></section>
@@ -12,7 +15,9 @@ document.querySelector('#app').innerHTML = `
 
 const initSharkwords = () => {
   let numWrong = 0;
-  const word = 'word';
+  const word = getRandomWord();
+
+  setSharkImage(document.querySelector("#shark-img"), numWrong);
 
   // for debugging:
   console.log(`[INFO] Correct word is: ${word}`);
--- src/word.js
+++ src/word.js
@@ -1,7 +1,14 @@
 let word;
 
-function setupWord(element, initWord) {}
+function setupWord(element, initWord) {
+  word = initWord;
+  for (const letter of word) {
+    element.innerText += `${_}`;
+  }
+}
 
 function isLetterInWord(letter) {}
 
 function revealLetterInWord(letter) {}
+
+export { setupWord, isLetterInWord, revealLetterInWord };
